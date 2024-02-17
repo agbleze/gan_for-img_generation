e6654e5b5c1ea99735c3eb08940484d83a970822
linus <lagbleze@st.ug.edu.gh>
2024-02-18 00:12:44 +0100

    Update conditional_gans.py

--- conditional_gans.py
+++ conditional_gans.py
@@ -8,3 +8,18 @@ from torchvision import transforms
 import torchvision.utils as vutils
 
 #%%
+female_images = Glob()
+
+img_folder = "C:/Users/agbji/Documents/codebase/gan_for_img_generation/male_female_face_images/"
+female_images = Glob(img_folder + "females/*.jpg") 
+male_images = Glob(img_folder + "males/*.jpg")
+
+
+#%%
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+
+def crop_images(folder):
+    pass
+
