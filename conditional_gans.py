fa45e3f5614bf2822166760e86fb576e7a628b6e
linus <lagbleze@st.ug.edu.gh>
2024-02-19 23:20:41 +0100

    Update conditional_gans.py

--- conditional_gans.py
+++ conditional_gans.py
@@ -8,7 +8,6 @@ from torchvision import transforms
 import torchvision.utils as vutils
 
 #%%
-
 img_folder = "C:/Users/agbji/Documents/codebase/gan_for_img_generation/male_female_face_images/"
 #female_images = Glob(img_folder + "females/*.jpg") 
 #male_images = Glob(img_folder + "males/*.jpg")
